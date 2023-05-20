3798345fbb47a9c152b95bebea9c2309df3cd27c
Iva Havranová <havranova.i@seznam.cz>
2023-05-20 14:35:57 +0200

    done

--- src/components/Rate/index.jsx
+++ src/components/Rate/index.jsx
@@ -4,17 +4,22 @@ import './style.css';
 const currencies = {
   USD: { CZK: 23.823 },
   EUR: { CZK: 24.74 },
-  GBP: { CZK: 29.067 }
+  GBP: { CZK: 29.067 },
 };
 
-const Rate = () => {
+// currencies.USD;
+// currencies["USD"]; toto znamená to samý to vejš
+// from = "USD"
+// currencies[from]
+
+const Rate = ({ from }) => {
   return (
     <div className="rate">
-      <div className="rate__currency">1 USD</div>
+      <div className="rate__currency">1 {from}</div>
       <div>=</div>
-      <div className="rate__value">22 CZK</div>
+      <div className="rate__value">{currencies[from].CZK} CZK</div>
     </div>
   );
 };
-
+// Upravte komponentu Rate tak, aby zobrazila správný kurz podle připravných dat v objektu currencies. Výslední kurz zobrazte v elementu .rate__value.
 export default Rate;
